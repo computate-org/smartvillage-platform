ca7d8421b297f4e08e238fc891ecbb90868c72fa
Christopher Tate <computate@computate.org>
2022-12-19 10:58:54 -0700

    Adding new models for BicycleStep and TrafficFlowObserved

---
+++ src/main/java/org/computate/smartvillageview/enus/model/traffic/fiware/trafficflowobserved/TrafficFlowObservedEnUSGenApiService.java
@@ -0,0 +1,42 @@
+package org.computate.smartvillageview.enus.model.traffic.fiware.trafficflowobserved;
+
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.codegen.annotations.ProxyGen;
+import io.vertx.serviceproxy.ServiceBinder;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.web.api.service.WebApiServiceGen;
+import io.vertx.ext.web.api.service.ServiceRequest;
+import io.vertx.ext.web.api.service.ServiceResponse;
+import io.vertx.ext.web.templ.handlebars.HandlebarsTemplateEngine;
+import io.vertx.core.WorkerExecutor;
+import io.vertx.pgclient.PgPool;
+import io.vertx.ext.auth.oauth2.OAuth2Auth;
+import io.vertx.ext.auth.authorization.AuthorizationProvider;
+
+/**
+ * Translate: false
+ * Gen: false
+ **/
+@WebApiServiceGen
+@ProxyGen
+public interface TrafficFlowObservedEnUSGenApiService {
+	static void registerService(EventBus eventBus, JsonObject config, WorkerExecutor workerExecutor, PgPool pgPool, WebClient webClient, OAuth2Auth oauth2AuthenticationProvider, AuthorizationProvider authorizationProvider, HandlebarsTemplateEngine templateEngine, Vertx vertx) {
+		new ServiceBinder(vertx).setAddress("smartabyar-smartvillage-enUS-TrafficFlowObserved").register(TrafficFlowObservedEnUSGenApiService.class, new TrafficFlowObservedEnUSApiServiceImpl(eventBus, config, workerExecutor, pgPool, webClient, oauth2AuthenticationProvider, authorizationProvider, templateEngine));
+	}
+
+	public void searchTrafficFlowObserved(ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void getTrafficFlowObserved(ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void patchTrafficFlowObserved(JsonObject body, ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void patchTrafficFlowObservedFuture(JsonObject body, ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void postTrafficFlowObserved(JsonObject body, ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void postTrafficFlowObservedFuture(JsonObject body, ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void putimportTrafficFlowObserved(JsonObject body, ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void putimportTrafficFlowObservedFuture(JsonObject body, ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void searchpageTrafficFlowObservedId(ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+	public void searchpageTrafficFlowObserved(ServiceRequest serviceRequest, Handler<AsyncResult<ServiceResponse>> eventHandler);
+}
--- src/main/resources/sql/db-create.sql
+++ src/main/resources/sql/db-create.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE IF NOT EXISTS SiteUser();
-ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS seeArchived boolean;
-ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS seeDeleted boolean;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS userId text;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS userName text;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS userEmail text;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS userFirstName text;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS userLastName text;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS userFullName text;
+ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS seeArchived boolean;
+ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS seeDeleted boolean;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS pk bigserial primary key;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS inheritPk text;
 ALTER TABLE SiteUser ADD COLUMN IF NOT EXISTS created timestamp with time zone;
@@ -25,10 +25,3 @@ ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS startDateTime timestamp w
 ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS startSeconds decimal;
 ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS endSeconds decimal;
 ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS stepSeconds decimal;
-ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS pk bigserial primary key;
-ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS inheritPk text;
-ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS created timestamp with time zone;
-ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS archived boolean;
-ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS deleted boolean;
-ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS sessionId text;
-ALTER TABLE TrafficSimulation ADD COLUMN IF NOT EXISTS userKey bigint;
